4ead2fe70ac2d23b417903db4dc820c9f8d72d2a
Harry Prior <harryprior@hotmail.co.uk>
2021-01-23 22:04:43 +0000

    Add google map component

---
+++ GroupMaps/ClientApp/src/components/GoogleMap.tsx
@@ -0,0 +1,37 @@
+﻿import '../styles/GoogleMap.css'
+import React, { useEffect } from 'react';
+import { Loader } from "@googlemaps/js-api-loader"
+
+
+type Center = {
+    lat: number,
+    lng: number
+}
+
+type Props = {
+    apiKey: string,
+    zoom?: number,
+    center: Center
+
+}
+
+const GoogleMap = (props: Props) => {
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: props.apiKey,
+            version: "weekly"
+        });
+        loader.load().then(() => {
+            (window as any).map = new (window as any).google.maps.Map(document.getElementById('map'), {
+                zoom: props.zoom === undefined ? 7 : props.zoom,
+                center: props.center
+            })
+        });
+    }, [props]);
+
+    return (
+        <div id="map"></div>
+        )
+}
+
+export default GoogleMap;
