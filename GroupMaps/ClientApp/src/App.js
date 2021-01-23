564cfc09464b08939d0bdd6f68e6fbc8a1dd6c4f
Harry Prior <harryprior@hotmail.co.uk>
2021-01-23 20:26:58 +0000

    Add project files.

---
+++ GroupMaps/ClientApp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import File from './file1';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+        <div>Hello</div>
+        <File/>
+        </div>
+        )
+}
+
+export default App;
---
+++ GroupMaps/ClientApp/src/file1.tsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const File = () => {
+    return (
+        <div> HArrrow! </div>
+    )
+}
+
+export default File
