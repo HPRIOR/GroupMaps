7dc0238abd2f96dd2402aebb44e7f24f67fb21f1
Harry Prior <harryprior@hotmail.co.uk>
2021-01-23 22:05:41 +0000

    Change App.js to .ts

---
+++ GroupMaps/ClientApp/src/App.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import GoogleMap from './components/GoogleMap';
+import './styles/App.css'
+import getMapsApiKey from './GetApiKey'
+
+const App = () => {
+
+    return (
+        <div id="main-window">
+            <GoogleMap apiKey={getMapsApiKey()} center={{ lat: 50.736129, lng: -1.98822 }} />
+        </div>
+        )
+}
+
+export default App;
