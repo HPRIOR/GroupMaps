d5a55b0cd15a77ed63dd6282392de1177c0481cf
Harry Prior <harryprior@hotmail.co.uk>
2021-01-26 18:05:36 +0000

    Add component to handle deletion, creation and representaiton of postcode input

---
+++ GroupMaps/ClientApp/src/components/PostCodeInput.tsx
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import Location from '../ObjectTypes/Location';
+
+type Props = {
+    locations: Location[],
+    postcode: string,
+    handleInputChange: Function,
+    addButtonCallBack: Function,
+    removeButtonCallBack: Function
+}
+
+const PostCodeInput = (props: Props) => {
+    return (
+        <div>
+            <ul>
+                {
+                    props.locations.map(i => (
+                        <li key={i.id}>
+                            {i.postcode}
+                            <button type="button" onClick={() => props.removeButtonCallBack(i.id)}>remove</button>
+                        </li>
+                    ))
+                }
+            </ul>
+            <input type="text" value={props.postcode} onChange={props.handleInputChange}></input>
+            <button type="button" onClick={props.addButtonCallBack}>add</button>
+        </div>
+    )
+}
+
+export default PostCodeInput;
