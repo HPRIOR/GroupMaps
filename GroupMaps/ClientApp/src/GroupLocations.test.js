dde0ad74e01b7f9086ead7a15eba9140e049dfab
Harry Prior <harryprior@hotmail.co.uk>
2021-01-26 12:13:54 +0000

    Test diagonal adjacent groups

--- GroupMaps/ClientApp/src/GroupLocations.test.js
+++ GroupMaps/ClientApp/src/GroupLocations.test.js
@@ -41,18 +41,69 @@ const test_locations_1 = [
     }
 ]
 
+const diagonal_location_groups = [
+    {
+        id: 1,
+        lat: 1,
+        lng: 1,
+        norm_lat: 91,
+        norm_lng: 181,
+        postcode: "N7 9RA"
+    },
+    {
+        id: 2,
+        lat: 0,
+        lng: 0,
+        norm_lat: 90,
+        norm_lng: 180,
+        postcode: "NW5 2NJ"
+    },
+    {
+        id: 3,
+        lat: 4,
+        lng: 4,
+        norm_lat: 94,
+        norm_lng: 184,
+        postcode: "N7 2G6"
+    },
+    {
+        id: 4,
+        lat: 5,
+        lng: 5,
+        norm_lat: 95,
+        norm_lng: 185,
+        postcode: "NW4 6RF"
+    }
+
+]
+
 it('Does not throw an error', () => {
     expect(() => groupLocations(test_locations_1, 1)).not.toThrow();
 });
 
-it('Produces two groups at distance of 1', () => {
+it('Produces two groups - distance: 1', () => {
     expect(groupLocations(test_locations_1, 1).length).toEqual(2);
-})
+});
 
-it('Produces 1 group at a distance equal to max(lat, lng)', () => {
+it('Produces one group - distance: max(lat, lng)', () => {
     expect(groupLocations(test_locations_1, 5).length).toEqual(1);
-})
+});
 
 it('Produces as many groups as locations where distance is half less than any distance between points', () => {
     expect(groupLocations(test_locations_1, 0.5).length).toEqual(4);
-})
+});
+
+it('Diagonal adjacent groups produce expected result - distance: 1', () => {
+    expect(groupLocations(diagonal_location_groups, 1).length).toEqual(2);
+});
+
+it('Diagonal adjacent groups produce one group - distance: max(lat,lng)', () => {
+    expect(groupLocations(diagonal_location_groups, 5).length).toEqual(1);
+});
+
+it('Diagonal adjacent groups produce expected result - distance: half less than any distance between points', () => {
+    expect(groupLocations(diagonal_location_groups, 0.5).length).toEqual(4);
+    
+});
+
+
