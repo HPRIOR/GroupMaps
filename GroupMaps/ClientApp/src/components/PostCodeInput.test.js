b5ed9675b22243a1916498eb383d9fc65dcde75b
Harry Prior <harryprior@hotmail.co.uk>
2021-01-30 21:15:03 +0000

    Add tests for PostCodeInput rendering

--- GroupMaps/ClientApp/src/components/PostCodeInput.test.js
+++ GroupMaps/ClientApp/src/components/PostCodeInput.test.js
@@ -1,9 +1,8 @@
-﻿import PostCodeInput from './PostCodeInput';
-import React from 'react';
-import {render} from '@testing-library/react'
+﻿import React from 'react';
+import PostCodeInput from './PostCodeInput' ;
+import { render } from '@testing-library/react';
 
-
-const test_locations = [
+const test_locations = () => [
     {
         id: 1,
         lat: 0,
@@ -37,4 +36,40 @@ const test_locations = [
         postcode: "NW4 6RF"
     }
 ]
+
+const postCodeInputProp = {
+    locations : test_locations,
+    postcode: "test",
+    handleInputChange: () => { },
+    addButtonCallBack: () => { },
+    removeButtonCallBack: () => {}
+
+}
+
+const Component = <PostCodeInput
+        locations={postCodeInputProp.locations()}
+        postcode={postCodeInputProp.postcode}
+        handleInputChange={postCodeInputProp.handleInputChange}
+        addButtonCallBack={postCodeInputProp.addButtonCallBack}
+        removeButtonCallBack={postCodeInputProp.removeButtonCallBack}
+    />
+
+test('Component is rendered', () => {
+    render(Component)
+});
+
+test('All locations are rendered', () => {
+    const { container } = render(Component);
+    const locations = container.querySelectorAll('li');
+    expect(locations).toHaveLength(4);
+});
+
+test('Correct postcodes are displayed', () => {
+    const { getByText } = render(Component);
+    getByText('N7 9RA')
+    getByText('NW5 2NJ')
+    getByText('N7 2G6')
+    getByText('NW4 6RF')
+});
+
 
