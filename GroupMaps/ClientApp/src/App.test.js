be35c58ce20fe8ca672c1cb0d39ab4f98b13bd07
Harry Prior <harryprior@hotmail.co.uk>
2021-02-05 09:00:22 +0000

    Add test for empty post code input

--- GroupMaps/ClientApp/src/App.test.js
+++ GroupMaps/ClientApp/src/App.test.js
@@ -1,12 +1,9 @@
 ﻿import App from './App';
 import React from 'react';
-import {render ,unmountComponentAtNode} from "react-dom"
+import { render, unmountComponentAtNode } from "react-dom"
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event"
 import { getByText, queryByText } from "@testing-library/react"
-//import MockedGoogleMap from './components/GoogleMap';
-
-
 
 let container = null
 beforeEach(() => {
@@ -21,10 +18,8 @@ afterEach(() => {
 });
 
 it('App renders', () => {
-    render(<App/>, container)
+    render(<App />, container)
 });
-
-
 
 it('Displays input post code - mock fetch', async () => {
     const fakePostCode = {
@@ -70,7 +65,7 @@ it('Displays input post code - mock fetch', async () => {
         }
     }
     // mock postCode fetch from API
-    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(fakePostCode)}))
+    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(fakePostCode) }))
     act(() => {
         render(<App />, container)
     });
@@ -84,8 +79,6 @@ it('Displays input post code - mock fetch', async () => {
 
     getByText(container, "N7 9RA")
 });
-
-
 
 it('Remove button removes postcode', async () => {
     const fakePostCode = {
@@ -131,7 +124,7 @@ it('Remove button removes postcode', async () => {
         }
     }
     // mock postCode fetch from API
-    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(fakePostCode)}))
+    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(fakePostCode) }))
     act(() => {
         render(<App />, container)
     });
@@ -143,17 +136,71 @@ it('Remove button removes postcode', async () => {
         await userEvent.click(addButton)
     });
 
-
     getByText(container, 'N7 9RA')
     const removeButton = container.querySelectorAll('.remove-post-code')[0];
     act(() => {
-         userEvent.click(removeButton);
+        userEvent.click(removeButton);
     })
 
     const tryGetRemovedPostCode = queryByText(container, 'N7 9RA')
     expect(tryGetRemovedPostCode).toBeNull();
-        
 });
 
+it('Add empty does nothing', async () => {
+    const fakePostCode = {
+        "status": 200,
+        "result": {
+            "postcode": "N7 9RA",
+            "quality": 1,
+            "eastings": 530690,
+            "northings": 185615,
+            "country": "England",
+            "nhs_ha": "London",
+            "longitude": -0.116337,
+            "latitude": 51.554291,
+            "european_electoral_region": "London",
+            "primary_care_trust": "Islington",
+            "region": "London",
+            "lsoa": "Islington 011D",
+            "msoa": "Islington 011",
+            "incode": "9RA",
+            "outcode": "N7",
+            "parliamentary_constituency": "Islington South and Finsbury",
+            "admin_district": "Islington",
+            "parish": "Islington, unparished area",
+            "admin_county": null,
+            "admin_ward": "Holloway",
+            "ced": null,
+            "ccg": "NHS North Central London",
+            "nuts": "Haringey and Islington",
+            "codes": {
+                "admin_district": "E09000019",
+                "admin_county": "E99999999",
+                "admin_ward": "E05000375",
+                "parish": "E43000209",
+                "parliamentary_constituency": "E14000764",
+                "ccg": "E38000240",
+                "ccg_id": "93C",
+                "ced": "E99999999",
+                "nuts": "UKI43",
+                "lsoa": "E01002767",
+                "msoa": "E02000564",
+                "lau2": "E05000375"
+            }
+        }
+    }
+    // mock postCode fetch from API
+    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(fakePostCode) }))
+    act(() => {
+        render(<App />, container)
+    });
+    const postCodeInput = container.querySelector('#post-code-input');
+    const postCodeButton = container.querySelector('#add-post-code');
 
-// empty text + add does not add post code
+    await act(async () => {
+        await userEvent.click(postCodeButton)
+    });
+
+    const tryGetRemovedPostCode = queryByText(container, 'N7 9RA')
+    expect(tryGetRemovedPostCode).toBeNull();
+});
