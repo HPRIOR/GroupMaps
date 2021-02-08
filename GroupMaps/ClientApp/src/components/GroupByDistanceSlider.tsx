4ec89484b3f153c2a0061a7cc4c7c4584a21ffea
Harry Prior <harryprior@hotmail.co.uk>
2021-02-08 17:06:43 +0000

    Add basic elements for slider

--- GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
+++ GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
@@ -1,8 +1,15 @@
 ﻿import React from 'react';
 
-const GroupByDistanceSlider = () => {
+type Props = {
+    maxRange: number,
+    onRangeChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    distance: number
+}
+
+const GroupByDistanceSlider = (props: Props) => {
     return (
         <>
+            <input type="range" min="0.0001" max={props.maxRange} value={props.distance} onChange={props.onRangeChange} />
         </>
         )
 }
