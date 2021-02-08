36ae0cb505a2e36b3b7970644efa1b7e1fbfb7d7
Harry Prior <harryprior@hotmail.co.uk>
2021-02-08 21:27:48 +0000

    add coments and debugging display of slider values

--- GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
+++ GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
@@ -5,11 +5,19 @@ type Props = {
     onRangeChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
     distance: number
 }
+/*
+ * things to consider:
+ * Max range will be the max distance between any point
+ * the slider should be disabled when there are no locations 
+ * step interval should change depending on the max distance
+ * 
+ */
 
 const GroupByDistanceSlider = (props: Props) => {
     return (
         <>
-            <input type="range" min="0.0001" max={props.maxRange} value={props.distance} onChange={props.onRangeChange} />
+            <input type="range" min="0.0001" max={props.maxRange} value={props.distance} onChange={props.onRangeChange} step='any'/>
+            <>{props.distance}</>
         </>
         )
 }
