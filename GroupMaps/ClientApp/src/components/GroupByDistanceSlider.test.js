0e97fb6537cfeacb864c376554aab5db806e8d09
Harry Prior <harryprior@hotmail.co.uk>
2021-02-09 17:40:50 +0000

    Add corrent init render test for slider

--- GroupMaps/ClientApp/src/components/GroupByDistanceSlider.test.js
+++ GroupMaps/ClientApp/src/components/GroupByDistanceSlider.test.js
@@ -1,9 +1,16 @@
 ﻿import GroupByDistanceSlider from './GroupByDistanceSlider' ;
 import React from 'react';
-import { render,  screen } from '@testing-library/react';
+import { act, render,  screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event"
 
 it('Renders Component', () => {
     render(<GroupByDistanceSlider />);
 });
+
+it('Renders correct correct starting value', () => {
+    act(() => {
+        render(<GroupByDistanceSlider distance="10" onRangeChange={() => {}} />)})
+    screen.getByText("10")
+});
+
 
