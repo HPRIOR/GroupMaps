afb1f91c8737faa52fd5f3124c2d554153f69f76
Harry Prior <harryprior@hotmail.co.uk>
2021-02-10 13:02:19 +0000

    add algorithm for calculating max distance between lat and lng

---
+++ GroupMaps/ClientApp/src/Modules/GetMaxDistance.ts
@@ -0,0 +1,20 @@
+﻿
+
+const GetDistanceFromLatLngInKm =
+    (latOne: number, lngOne: number, latTwo: number, lngTwo: number) => {
+        const R = 6371;
+        const degreeLat = degreeToRad(latTwo - latOne);
+        const degreeLng = degreeToRad(lngTwo - lngOne);
+        const a =
+            Math.sin(degreeLat / 2) * Math.sin(degreeLat / 2) +
+            Math.cos(degreeToRad(latOne)) * Math.cos(degreeToRad(latTwo)) *
+            Math.sin(degreeLng / 2) * Math.sin(degreeLng / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+
+
+}
+
+const degreeToRad = (degree: number) => degree * (Math.PI / 180);
+
+export default GetDistanceFromLatLngInKm;
