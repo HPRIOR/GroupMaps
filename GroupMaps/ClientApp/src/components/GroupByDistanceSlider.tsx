2ea8b0167c1265413c98f04407d6693e17e27dd6
Harry Prior <harryprior@hotmail.co.uk>
2021-02-10 11:01:16 +0000

    Change minimum range to account for smaller distances

--- GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
+++ GroupMaps/ClientApp/src/components/GroupByDistanceSlider.tsx
@@ -16,7 +16,7 @@ type Props = {
 const GroupByDistanceSlider = (props: Props) => {
     return (
         <>
-            <input type="range" min="0.0001" max={props.maxRange} value={props.distance} onChange={props.onRangeChange} step='any' />
+            <input type="range" min="0.000001" max={props.maxRange} value={props.distance} onChange={props.onRangeChange} step='any' />
             <>{props.distance}</>
         </>
     );
