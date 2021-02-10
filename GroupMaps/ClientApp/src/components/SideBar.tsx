7cc652a8be85f3df55add762e50dc69109854b07
Harry Prior <harryprior@hotmail.co.uk>
2021-02-10 13:57:44 +0000

    Add sidebar component

---
+++ GroupMaps/ClientApp/src/components/SideBar.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { LocationGroup } from '../ObjectTypes/LocationGroup';
+
+type Props = {
+    locationGroups: LocationGroup;
+}
+
+const SideBar = (props: Props) => {
+    return (
+        <></>
+        )
+}
+
+export default SideBar
